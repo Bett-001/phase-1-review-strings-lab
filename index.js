6ce4babbeb23a32f1caa2f3e54e0f67198ea5bbc
Tonnie-debug-svg <tonniekbett@gmail.com>
2025-09-10 21:17:12 +0300

    Completed Flatbook greeting lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// Defining the user
+const currentUser = 'Grace Hopper';
+
+// Create a welcome message using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create welcome message in capital letters
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting with only the first initial of the user
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
